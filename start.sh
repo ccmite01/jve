fde9c66ac06d3991d795add218d89b11920f6078
ccmite01 <ccmite01@gmail.com>
2019-12-12 07:36:18 +0900

    20191212

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ fi
 
 if [ ! -d /var/www/html/console ]
     then
-    tar -x -v -f /console.tar.gz /var/www/html/
+    tar -x -v -f /console.tar.gz -C /var/www/html/
     chmod +x /var/www/html/console/*.sh
     sed -i "s/ccmite/${MC_INSTANCE_NAME}/g" /var/www/html/console/config/config.php
 fi
